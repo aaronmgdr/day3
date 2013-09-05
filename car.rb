6fb73a39adcea99179caf9415b9285878e404193
MakerSquare Student <students@themakersquare.com>
2013-09-05 17:59:07 -0500

    gives price to fuelup

--- car.rb
+++ car.rb
@@ -6,12 +6,20 @@ class Car
 		puts "the initialize method is running automatically"
 	end
 
-	def get_info
+	def info
 		return "I'm a car. I've driven #{@distance} miles and have #{@fuel} gallons of gas left"
 	end
-	def drive(miles) 
-		@distance += miles 
-		@fuel -= (miles/20.0) 
-		
+	def drive(miles)
+		if (@fuel > 0) && (miles < 200)
+			@distance += miles 
+			@fuel -= (miles/20.0) 
+		else 
+			
+		end
+	end
+	def fuel_up
+		gallons = (10.0 - @fuel) 
+		cost = gallons * 3.50
+		puts "It will cost you #{cost} to fill up #{gallons}"
 	end
 end
